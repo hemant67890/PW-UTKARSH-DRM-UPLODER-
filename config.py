f20d6c7d2e2deefda957a9ad2f90cfa06bce8d94
hemant67890 <godarasubhash337@gmail.com>
2024-12-30 00:23:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ API_ID = API_ID = 26512964
 
 API_HASH = os.environ.get("API_HASH", "e5d187c6c7a0919ccb8866f76f655701")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7510608040:AAE8gLvOPW-vqehLwN0qrEs5nryHrimEsbk")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-PASS_DB = int(os.environ.get("PASS_DB", "mongodb+srv://sujay5372192:sujay5372192@cluster00001.zivqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster00001"))
+PASS_DB = int(os.environ.get("PASS_DB", ""))
 
-OWNER = int(os.environ.get("OWNER", 2052075731))
-STRING = int(os.environ.get("STRING", AQFwCeIAOz1FS7JecfYU8zMZdNCoey8c3cbpOlG0CmPPY9mXBXyG2C0_Uf83cWS_dI38I16qpCyuggIpwc2LrcYQtUXEbyjxtfRWl3jif61NDbq95dMqvSLJkYz6xGaPas5qCfMubSwdkxgaFc_ejU1A5Fglp4RsPKnH_G4OjF_wEUAzNiWn0PhTVlUT-au6thdjfR-hsPvGUnksl4bTRb4BfR0cFsh0X8Tg14_KGJRMBGUN2URG1PO1ATGpiFDPmUDFco2SwQIW6VadpUUROTUD70z0SnAvQ7aZMD7MvpovTdHO8YJVu0GiMT5qJZKtNGHUzKj4rdxiKdHoA0_9z5h2pIR20QAAAAGrb7QLAA))
+OWNER = int(os.environ.get("OWNER", ))
+STRING = int(os.environ.get("STRING", ))
 
-LOG = -1002111247909
+LOG = 
 
